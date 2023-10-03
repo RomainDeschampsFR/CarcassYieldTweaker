f1c15a5f38de32451ffc2589204fb45001dc3ac8
OrangeCrushMan <84152414+OrangeCrushMan@users.noreply.github.com>
2023-10-03 17:02:33 -0600

    Add files via upload

---
+++ src/Settings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModSettings;
+
+namespace UseableMeatMod
+{
+    internal static class Settings
+    {
+        internal static void OnLoad()
+        {
+            Settings.options = new UseableMeatModSettings();
+            Settings.options.AddToModSettings("Useable Meat Mod Settings");
+        }
+        internal static UseableMeatModSettings options;
+    }
+}
